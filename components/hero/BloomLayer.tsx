2bead25ed3927bc19648651bbd2eff9ddfcd4952
xnicolejavier-art <x.nicolejavier@gmail.com>
2026-07-04 09:26:04 -0400

    Create BloomLayer.tsx

---
+++ components/hero/BloomLayer.tsx
@@ -0,0 +1,16 @@
+export default function BloomLayer() {
+  return (
+    <>
+      <div
+        aria-hidden="true"
+        className="bloom-layer"
+        id="bloom-primary"
+      />
+      <div
+        aria-hidden="true"
+        className="bloom-layer-diamond"
+        id="bloom-diamond"
+      />
+    </>
+  );
+}
