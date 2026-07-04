5bd90bbf591212303be6e1398ec2ab1581771533
xnicolejavier-art <x.nicolejavier@gmail.com>
2026-07-04 10:00:27 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/hero/HeroSection";
 import GrainOverlay from "@/components/hero/GrainOverlay";
+import TaglineSection from "@/components/tagline/TaglineSection";
 
 export default function Home() {
   return (
     <main>
       <GrainOverlay />
       <HeroSection />
+      <TaglineSection />
     </main>
   );
 }
